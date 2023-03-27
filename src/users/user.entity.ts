4cf76098b690234e67e043a0ad9e7e86beeaa061
Test <test@Mangals-MacBook-Pro.local>
2023-03-27 23:32:13 +0530

    hooks to confirm whether user data is inserted removed or updated

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,11 +1,33 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   email: string;
+
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+  @AfterUpdate()
+  logUpdate() {
+    console.log('User Updated with id', this.id);
+  }
+  @AfterRemove()
+  logRemove() {
+    console.log('User Removed with id', this.id);
+  }
 }
