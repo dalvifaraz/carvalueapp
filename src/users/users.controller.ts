0552d2b1172c0c9343f9de5bd71eb05482d374a1
Test <test@Mangals-MacBook-Pro.local>
2023-03-28 23:33:55 +0530

    execeptional handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,7 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { NotFoundException } from '@nestjs/common';
 
 @Controller('auth')
 export class UsersController {
@@ -22,8 +23,12 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+    return user;
   }
 
   @Get()
