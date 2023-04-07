faefdf4cfafe0ed12d7bf5160ea3c6492e0dead9
dalvifaraz <dalvifaraz@gmail.com>
2023-04-08 02:24:42 +0530

    production config update

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,9 @@
 const dbConfig = {
   synchronize: false,
+  migrations: ['migrations/*.js'],
+  cli: {
+    migrationsDir: 'migrations',
+  },
 };
 
 switch (process.env.NODE_ENV) {
@@ -18,6 +22,15 @@ switch (process.env.NODE_ENV) {
     });
     break;
   case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
     break;
   default:
     throw new Error('Unknown Environment');
