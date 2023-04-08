706c5943b9eaaf608f6f9f2025a0b9680aef16b7
dalvifaraz <dalvifaraz@gmail.com>
2023-04-08 23:20:41 +0530

    datasource update

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,10 +1,14 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
-  type: 'sqlite',
-  database: 'db.sqlite',
-  entities: ['**/*.entity.js'],
+  type: 'postgres',
+  host: 'postgres://carvalueapp_user:BN27TCbzf0pVONKqrR4bxN7f9hhsWva2@dpg-cgo8i48u9tun42o0llh0-a/carvalueapp',
+  port: 5432,
+  username: 'carvalueapp_user',
+  password: 'BN27TCbzf0pVONKqrR4bxN7f9hhsWva2',
+  database: 'carvalueapp',
   synchronize: true,
+  entities: ['**/*.entity.js'],
   migrations: ['./db/migrations/*.js'],
 };
 
