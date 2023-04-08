df8eac45149732d729f289a661bc2ef7cf06ea3f
dalvifaraz <dalvifaraz@gmail.com>
2023-04-09 00:37:02 +0530

    host name updat

--- db/data-source.ts
+++ db/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'postgres://carvalueapp_user:BN27TCbzf0pVONKqrR4bxN7f9hhsWva2@dpg-cgo8i48u9tun42o0llh0-a/carvalueapp',
+  host: 'dpg-cgo8i48u9tun42o0llh0-a',
   port: 5432,
   username: 'carvalueapp_user',
   password: 'BN27TCbzf0pVONKqrR4bxN7f9hhsWva2',
